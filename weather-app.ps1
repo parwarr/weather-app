a65a741dd6adea9924c30b291eba1d345206752b
Saranhan <saranhanthurairasa@gmail.com>
2023-09-15 13:01:19 +0200

    feat: weather api implementation

--- weather-app.ps1
+++ weather-app.ps1
@@ -10,5 +10,34 @@
 
 Clear-Host
 
+#this variable is for the api link
+$api = "https://api.open-meteo.com/v1/forecast?"
+
+$lat = 46.9481
+$long = 7.4474
+
+# here is the url variable that will give out the information in terminal
+$url = Invoke-WebRequest -uri "$api&latitude=$lat&longitude=$long&current_weather=true"
+$urlF = Invoke-WebRequest -uri "$api&latitude=$lat&longitude=$long&current_weather=true&temperature_unit=fahrenheit" 
 
 
+$result = $url | ConvertFrom-Json
+$temp = $result.temperature.value;
+
+#output to terminal
+Write-Host "$temp"
+
+#choose if you want the output in celsius or fahrenheit
+
+#write-host "Celsius or Fahrenheit"
+#read-host ""
+    if (Read-Host "Celsius") {
+        <# Action to perform if the condition is true #>
+            write-host "$url"
+    }
+    elseif (Read-Host "Fahrenheit") {
+        <# Action when this condition is true #>
+    }{
+        <# Action when all if and elseif conditions are false #>
+            write-host "$urlF"
+    }
