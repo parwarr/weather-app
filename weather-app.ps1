8226263e57a151846e115c0aec68cbf67ece3a9d
parwarr <parwar.habasch@gmx.de>
2023-09-15 22:29:01 +0200

    feat: added comments and updated some stuff

--- weather-app.ps1
+++ weather-app.ps1
@@ -1,6 +1,6 @@
 #Modul 129 LB1
 #Project: Weather App
-#Author: sth134864@stud.gibb.com |
+#Author: sth134864@stud.gibb.com | hpa134085@stud.gibb.ch
 #
 #ToDo
 #1. Get information from WeatherAPI
@@ -11,39 +11,54 @@
 Clear-Host
 
 #Varibales
-$geocodeURL="https://geocode.maps.co/search?q=";
-$WHEATERAPI= "https://api.open-meteo.com/v1/forecast";
+$geocodeApi="https://geocode.maps.co/search?q=";
+$weatherApi= "https://api.open-meteo.com/v1/forecast";
 
 function Get-GeoCode {
   param (
-    [string]$City
+    [string]$city
   )
 
   # Make the web request and store the JSON response in $geocode
-  $geocodeResponse = Invoke-WebRequest -Uri "${geocodeURL}${City}" -ErrorAction Stop | Select-Object -ExpandProperty Content
+  $geoCodeResp = Invoke-WebRequest -Uri "${geocodeApi}${city}" -ErrorAction Stop | Select-Object -ExpandProperty Content
 
-  # Convert the JSON response to a PowerShell object
-  $jsonObject = $geocodeResponse | ConvertFrom-Json
+  try {
+    # Convert the JSON response to a Powershell object
+    $jsonObject = $geoCodeResp | ConvertFrom-Json
 
-  # Access the lat and lon properties
-  $lat = $jsonObject.lat[0]
-  $lon = $jsonObject.lon[0]
+    # Get the lat and lon(coordinates) values from the JSON object
+    $lat = $jsonObject.lat[0]
+    $lon = $jsonObject.lon[0]
 
-  # Output the latitude and longitude
-  Write-Host "Latitude: $lat"
-  Write-Host "Longitude: $lon"
+    # Check if lat and lon are null if so throw an error
+    if ($null -eq $lat -or $null -eq $lon) {
+      throw "Could not find lat and lon for $city"
+    } 
+
+    # Return the lat and lon values
+    return $lat, $lon
+  }
+  catch {
+     # Catch errors $_ is a special variable integrated in Powershell that contains the error message
+    Write-Host "Error occurred: $_ "
+  }
+
 }
 
 try {
+    # Get the city from the user
    $city = Read-Host "Enter City"
+   # Call GeoCode function with the city the user inserted. 
   Get-GeoCode($city)
-  $result = Invoke-WebRequest -Uri "${WHEATERAPI}?latitude=${lat}&longitude=${lon}&current_weather=true";
 
-  Write-Host $result
+#   $result = Invoke-WebRequest -Uri "${weatherApi}?latitude=${lat}&longitude=${lon}&current_weather=true";
+
+#   Write-Host $result
 
 }
 catch {
-  Write-Host "Error occurred: $_"
+ # Catch errors $_ is a special variable integrated in Powershell that contains the error message
+  Write-Host "Error occurred: $_ "
 }
 
 
