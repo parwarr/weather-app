ea5af6c6cbb455b135f2e26b9af34bd09c1e8cd6
Saranhan <saranhanthurairasa@gmail.com>
2023-09-15 12:09:35 +0200

    feat: header for script

---
+++ weather-app.ps1
@@ -0,0 +1,14 @@
+#Modul 129 LB1
+#Project: Weather App
+#Author: sth134864@stud.gibb.com |
+#
+#ToDo
+#1. Get information from WeatherAPI
+#2. Define the Timespan that of the Weather that needs to be shown
+#3. Get Temparatur and state of the Weather
+#4. Create nice Terminal appearance for entering the City
+
+Clear-Host
+
+
+
